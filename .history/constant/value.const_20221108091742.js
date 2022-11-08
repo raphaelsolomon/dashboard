353e48bd3dcaddcdb598990c9e03f9869ce56d93
raphaelsolomon <phoenixk54@gmail.com>
2022-11-08 09:23:33 +0100

    good

---
+++ .history/constant/value.const_20221108091742.js
@@ -0,0 +1,139 @@
+const { check } = require("express-validator");
+exports.LOGISTIC_HEADER = [
+  "Id",
+  "Consignor",
+  "Phone No",
+  "Item",
+  "Date Of Order",
+  "Pickup Address Street",
+  "Pickup Address Area",
+  "Pickup Time",
+  "Consignee",
+  "Consignee Phone No",
+  "Delivery Address Str",
+  "Delivery Address Area",
+  "Delivery Time",
+  "Mode Of Payment",
+  "Rider",
+  "Amount",
+  "Payment Status",
+  "Status",
+];
+
+exports.LOGISTIC_ATTRIBUTE = [
+  "id",
+  "consignor",
+  "phone_number",
+  "itemToBeDelivered",
+  "dateOfOrder",
+  "pickupaddress_str",
+  "pickupaddress_area",
+  "pickup_time",
+  "consignee",
+  "c_phone_number",
+  "c_deliveryAddress_str",
+  "c_deliveryAddress_area",
+  "c_deliveryTime",
+  "mode_of_pay",
+  "rider",
+  "amount_paid",
+  "payment_status",
+  "status",
+];
+
+exports.PLASTIC_HEADER = [
+  "Id",
+  "Retrieved From",
+  "Date",
+  "Plastic Size",
+  "Tonnage",
+  "Product",
+  "Volume Of Plastics",
+  "Initial Content",
+  "Manufacturer",
+];
+
+exports.PLASTIC_ATTRIBUTE = [
+  "id",
+  "retrieved_from",
+  "date",
+  "plastic_size",
+  "tonnage",
+  "product",
+  "volume_of_plastics",
+  "initial_content",
+  "manufacturer",
+];
+
+exports.SORT_HEADER = [
+  "Id",
+  "Date",
+  "Sorted Pet Plastic Weight",
+  "Sorted Data",
+  "Sorted Color Cap Labels",
+  "Units",
+];
+
+exports.SORT_ATTRIBUTES = [
+  "id",
+  "date",
+  "plastic_weight",
+  "Sorted Data",
+  "Sorted Color Cap Labels",
+  "Units",
+];
+
+exports.COMMODITY_HEADER = [
+  "ID",
+  "Purchased Date",
+  "Purchased Cost",
+  "Customer Name",
+  "Phone Number",
+  "Commodity",
+  "Commodity Size",
+  "Commodity Color",
+  "Sales Date",
+  "Sales Cost",
+  "Delivery Street",
+  "Purchasing Gender",
+  "Mode of Payment",
+  "Delivery Location",
+  "Mode of Transaction",
+  "Advertising Medium",
+  "Status",
+];
+
+exports.COMMODITY_ATTRIBUTE = [
+  "id",
+  "purchased_date",
+  "purchased_cost",
+  "customer_name",
+  "phone_no",
+  "commodity",
+  "commodity_size",
+  "commodity_color",
+  "sales_date",
+  "sales_cost",
+  "delivery_street",
+  "purchasing_gender",
+  "mode_payment",
+  "delivery_location",
+  "mode_transaction",
+  "advertising_medium",
+  "status",
+];
+
+exports.VALIDATE = [
+  check("email", "Email is not valid").isEmail().normalizeEmail(),
+  check("phone_number", "Phone Number is not valid").isLength({
+    min: 11,
+    max: 11,
+  }),
+  check("password", "Password is not valid")
+    .isLength({ min: 6 })
+    .normalizeEmail(),
+];
+
+exports.WEMABODS_ATTRIBUTE = [];
+
+exports.WEMABODS_HEADER = [];
