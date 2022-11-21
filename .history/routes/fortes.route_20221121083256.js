8797aa2d5826c5d999ae86b5af8b80b21fdac9af
raphaelsolomon <phoenixk54@gmail.com>
2022-11-21 08:34:05 +0100

    good

--- .history/routes/fortes.route_20221121083256.js
+++ .history/routes/fortes.route_20221121083256.js
@@ -8,7 +8,7 @@ route.get('/', async (req, res) => {
 
 route.post('/submit', async (req, res) => {
     return Fort.create(req.body).then((_) => {
-        return res.status(200).json('succesfully submitted...'); 
-    }).catch((err) => res.status(200).json('error occurred when submitting...'));
+        return res.status(200).json('succesfully created'); 
+    });
 })
 module.exports = route;  
--- .history/routes/fortes.route_20221121083330.js
+++ .history/routes/fortes.route_20221121083330.js
@@ -9,6 +9,6 @@ route.get('/', async (req, res) => {
 route.post('/submit', async (req, res) => {
     return Fort.create(req.body).then((_) => {
         return res.status(200).json('succesfully submitted...'); 
-    }).catch((err) => res.status(200).json('error occurred when submitting...'));
+    }).catch((err) => res.status(200).json('succesfully submitted...'); );
 })
 module.exports = route;  
