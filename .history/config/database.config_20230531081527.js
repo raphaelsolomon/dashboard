10316feb761527f09317f16a9b14cd601d504484
raphaelsolomon <phoenixk54@gmail.com>
2023-05-31 08:22:36 +0100

    first commit

---
+++ .history/config/database.config_20230531081527.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+var sequelize;
+if (process.env.NODE_ENV === 'production') {
+   // sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSERNAME, process.env.DBPASS, { dialect: 'mysql', host: process.env.DBHOST });
+    sequelize = new Sequelize("id20829920_dech_database", "id20829920_phoenixk54", "Available1.", { dialect: 'mysql', host: process.env.DBHOST });
+} else {
+    //sequelize = new Sequelize('dechcons_dashboard', 'root', 'root', { dialect: 'mysql', host: 'localhost' });
+    sequelize = new Sequelize("u885646101_dech_database", "u885646101_phoenixk54", "Available1.", { dialect: 'mysql', host: "149.100.151.52" });
+}
+module.exports = sequelize;
